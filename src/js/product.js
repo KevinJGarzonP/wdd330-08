77f070db9d8ed103b04449baec396f2df38e896d
AndreaGarzon~ <gar21082@byui.edu>
2023-05-04 23:03:31 -0500

    W2 02

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,13 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
+
 
 const productId = getParam('product');
 console.log(findProductById(productId));
+
+productDetails(productId);
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
