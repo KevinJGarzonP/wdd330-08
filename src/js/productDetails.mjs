3390c0c327b621d532c4ded0a549311fdb6aa463
AndreaGarzon~ <gar21082@byui.edu>
2023-05-07 00:08:32 -0500

    productDetails file completed

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,18 +1,30 @@
+import { setLocalStorage } from "./utils.mjs";
+import { findProductById } from "./productData.mjs";
+
 export default async function productDetails(productId) {
-    
+  let product = await findProductById(productId);
+  renderProductDetails(product);
+  // add listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+}
+
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
 }
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-    const product = await findProductById(e.target.dataset.id);
-    addProductToCart(product);
-  }
+  const product = await findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
   
-// add listener to Add to Cart button
-document
-.getElementById("addToCart")
-.addEventListener("click", addToCartHandler);
-
-function renderProductDetails() {
-    
+function renderProductDetails(product) {
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productType").innerText = product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColor").innerText = product.Colors[0].ColorName;
+  document.querySelector("#productDescription").innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
 }
